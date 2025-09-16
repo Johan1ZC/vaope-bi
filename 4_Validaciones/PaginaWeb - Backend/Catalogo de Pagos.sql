b18a404cc5aba5092ae2b350615fcc482af7ac8b
Johan1ZC <johan@vaope.com>
2025-09-16 14:56:19 -0500

    Version_8.9

--- 4_Validaciones/PaginaWeb - Backend/Catalogo de Pagos.sql
+++ 4_Validaciones/PaginaWeb - Backend/Catalogo de Pagos.sql
@@ -40,6 +40,7 @@ from vaope2.sale_payments a
 left join vaope2.payment_methods b on a.payment_method_id = b.id
 group by b.name,a.validation_comments
 
+select * from vaope2.sale_payments a
 
 
 
@@ -141,6 +142,16 @@ use vaope;
 select a.id,a.payment_method_id,b.name,a.validation_comments,a.entidad_financiera, procesador_wallet,red_tarjeta,producto
 from vaope.tmp_payments_parse  a
 left join vaope2.payment_methods b on a.payment_method_id = b.id
+
+-- Red_tarjeta = Yape y procesador_wallet = Yape -- NIUBIZ - ON LINE = yape
+
+-- Agrupacion limpia
+use vaope;
+select b.name,a.entidad_financiera, procesador_wallet,red_tarjeta,producto,count(1) Q
+from vaope.tmp_payments_parse  a
+left join vaope2.payment_methods b on a.payment_method_id = b.id
+group by b.name,a.entidad_financiera, procesador_wallet,red_tarjeta,producto
+order by 6 desc
 
 use vaope;
 select * from vaope.tmp_payments_parse 
